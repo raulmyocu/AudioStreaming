ae54ab7baaa1de6b91eb36ae9bc720b9d1cee151
raulmyocu <rauloctaviomurcia@gmail.com>
2019-08-17 19:44:37 -0700

    Input server and port

--- Client.py
+++ Client.py
@@ -1,8 +1,8 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server = socket.gethostname()
-port = int(input("Server Port"))
+server = input("Server: ")
+port = int(input("Server Port: "))
 
 s.connect((server, port))
 
